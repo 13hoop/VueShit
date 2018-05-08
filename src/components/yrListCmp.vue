08d0f00afead07c5bf83834a818b3631b9979718
13hoop <zhanxin13@gmail.com>
2018-05-08 09:14:59 +0800

    组件嵌套组件

---
+++ src/components/yrListCmp.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="list">
+        <span class="list-title">{{ titleStr }}</span>
+        <!-- <Btn :msg=msg @click="btnClicked" class="small"></Btn> -->
+        <component :is="currentView" :msg=msg @click="clicked" class="small" keep-alive></component>
+    </div>
+</template>
+
+<script>
+import YRBtn from "../components/yrButtonCmp"
+import YRArrow from "../components/yrArrowCmp"
+export default {
+  props: {
+    msg: {
+      default: "下载"
+    },
+    titleStr: {
+      default: "cell Title here!"
+    },
+    currentView: {
+        default: 'Btn'
+    }
+  },
+  methods: {
+      clicked: function() {
+
+      }
+  },
+  components: {
+    Btn: YRBtn,
+    Arrow: YRArrow
+  }
+};
+</script>
+
+<style>
+.list {
+  border: 1px solid red;
+  background: #eeeeee;
+}
+
+span {
+  color: #000;
+  font-size: 20px;
+}
+</style>
---
+++ src/pages/yrListPage.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="listEl">
+   <YRList titleStr="1111" msg="hahahah"></YRList> 
+   <YRList titleStr="222" @click="test" msg="2222"></YRList> 
+   <YRList titleStr="333" ref="child1"  currentView="Arrow" msg="333333"></YRList> 
+</div>
+</template>
+
+<script>
+import YRList from '../components/yrListCmp'
+export default {
+    name: "listEl",
+    components: {
+        "YRList": YRList
+    },
+    beforeCreate:function(){},  //组件实例化之前
+    created:function(){},       //组件实例化了
+    beforeMount:function(){},   //组件写入dom结构之前
+    mounted:function(){         //组件写入dom结构了
+        console.log(this.$el);
+        console.log(this.$children);
+        console.log(this.$refs);
+        console.log(this.$refs.child1.msg);
+    },
+    methods: {
+        test: function() {
+
+        }
+    },
+    beforeUpdate:function(){},//组件更新前
+    updated:function(){},//组件更新比如修改了文案
+    beforeDestroy:function(){},//组件销毁之前
+    destroyed:function(){}//组件已经销毁
+}
+</script>
+
+
+
+<style>
+
+</style>
